2bb1f67d49e6f0ad250049329ff3f2426cbf07a0
Nisham Ghimire <nishamghimire5@gmail.com>
2021-09-19 14:53:03 +0545

    added functions declarations for files

--- Bank_Management_Sys_.cpp
+++ Bank_Management_Sys_.cpp
@@ -106,55 +106,67 @@ void bank :: openAccount(){
         cout << id << setw(6) << acc_num << setw(6) << name << setw(6) << address << setw(6) << type << setw(6) << balance << endl;
     }
 
+/////////////////////////functions declarations///////////////////
+    void writeAccount(); //writes record in file
+    void displayAccount(); //displays account details
+    void modifyAccount(int); //modifies records of file
+    void deleteAccount(int); //deletes record of file
+    void displayAll(); //displays all accounts
+    void DepositWithdraw(int, int); //adds or deducts transactions for the given account
+    void FirstScreen(); //show the intro screen
 
-int main(){
-    //program execute form here |-->
-    char choice2;
-    int choice;
-    bank object;
-    do{
-    cout << "1.  Create an account \n";
-    cout << "2.  Deposit Money \n";
-    cout << "3.  Withdraw Money \n";
-    cout << "4.  Display Account \n";
-    cout << "5.  Exit \n";
-    cout << "Enter your choice : ";
-    cin >> choice;
-    system("cls");
+    int main()
+    {
+        //program execute form here |-->
+        char choice2;
+        int choice;
+        bank object;
+        do
+        {
+            cout << "1.  Create an account \n";
+            cout << "2.  Deposit Money \n";
+            cout << "3.  Withdraw Money \n";
+            cout << "4.  Display Account \n";
+            cout << "5.  Exit \n";
+            cout << "Enter your choice : ";
+            cin >> choice;
+            system("cls");
 
-    switch(choice){
-        case 1: 
-            cout<< "1. Create account \n";
-            object.openAccount();
-        break;
+            switch (choice)
+            {
+            case 1:
+                cout << "1. Create account \n";
+                object.openAccount();
+                break;
 
-        case 2:
-            cout << "2. Deposit Money \n";
-        object.depositMoney();
-        break;
-        
-        case 3:
-            cout<<"3. Withdraw Money \n";
-            object.withdrawMoney();
-            break;
-            
-        case 4:
-            cout<<"4. Display Account \n";
-            object.displayAccount();
-            break;
-        case 5:
-            exit(1);
-        default:
-            cout << "Invalid input \n";
-            continue;
-    }
+            case 2:
+                cout << "2. Deposit Money \n";
+                object.depositMoney();
+                break;
 
-    cout << "Do you want to go back to menu? (Y/N) : \n";
-    choice2 =getch();
+            case 3:
+                cout << "3. Withdraw Money \n";
+                object.withdrawMoney();
+                break;
 
-    if (choice2=='N' || choice2=='n') exit(0);
+            case 4:
+                cout << "4. Display Account \n";
+                object.displayAccount();
+                break;
+            case 5:
+                exit(1);
+            default:
+                cout << "Invalid input \n";
+                continue;
+            }
 
-    } while (choice2 ='Y' || choice2=='y');
-    
-    return 0;
+            cout << "Do you want to go back to menu? (Y/N) : \n";
+            choice2 = getch();
+
+            if (choice2 == 'N' || choice2 == 'n')
+                exit(0);
+
+        } while (choice2 = 'Y' || choice2 == 'y');
+
+        return 0;
 }
